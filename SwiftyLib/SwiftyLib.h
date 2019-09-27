4250953fca1a3aa2ae82ebb22b22a2795ab3f8b3
Sambasiva Rao Dodigam <sambasiva.r.dodigam@kp.org>
2019-09-26 23:13:35 -0400

    Initial Commit

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Sambasiva Rao Dodigam on 9/26/19.
+//  Copyright © 2019 Kaiser Permanente. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
