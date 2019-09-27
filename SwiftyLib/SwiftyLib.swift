d8cbd4b5bc410262de2d7741e7b514a632a0b258
Sambasiva Rao Dodigam <sambasiva.r.dodigam@kp.org>
2019-09-26 23:38:15 -0400

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Sambasiva Rao Dodigam on 9/26/19.
+//  Copyright © 2019 Kaiser Permanente. All rights reserved.
+//
+
+import Foundation
+
+class SwiftyLib: NSObject {
+    func add(a: Int, b: Int) -> Int {
+        return a+b
+    }
+
+    func sub(a: Int, b: Int) -> Int {
+        return a-b
+    }
+}
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -19,16 +19,16 @@ class SwiftyLibTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAdd() {
+        let swiftyLib = SwiftyLib()
+        let total = swiftyLib.add(a: 10, b: 15)
+        XCTAssertEqual(total, 25)
+    }
+    func testSub() {
+        let swiftyLib = SwiftyLib()
+        let total = swiftyLib.sub(a: 20, b: 15)
+        XCTAssertEqual(total, 5)
     }
 
 }
